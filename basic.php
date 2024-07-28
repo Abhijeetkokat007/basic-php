17f9abbfd19ac3e20c1dbd10b6fa0beb9ed414ae
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-07-28 22:04:02 +0530

    added basic PHP

---
+++ basic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic PHP</title>
+</head>
+<body>
+    hello php this is test
+
+    <?php 
+    echo "
+    <h1> this is php code </h1>
+    <markdown> this is markdown </markdown>
+    "
+    ?>
+    <?php 
+    echo "
+    <h1> this is php code </h1>
+    <markdown> this is markdown </markdown>
+    "
+    ?>
+    <div>
+</div>
+</body>
+</html>
